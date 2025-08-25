058fbb1483e439d506ef43c8587e89478657e1f5
Maoji Programming <maoji.programming@gmail.com>
2025-08-25 13:58:26 -0700

    Update ECN selection, fix bugs

--- logic/automation.py
+++ logic/automation.py
@@ -2,6 +2,7 @@ import selenium
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import NoSuchElementException
@@ -38,7 +39,9 @@ class Automation:
         print("start login...")
         options = webdriver.ChromeOptions()
         options.binary_location = chrome_path
-        self.driver = webdriver.Chrome(executable_path=driver_path, options=options)
+        print(driver_path)
+        service = Service(executable_path=driver_path)
+        self.driver = webdriver.Chrome(service=service, options=options)
         
         self.driver.get(self.config["login_url"])  # Replace with actual UCS login URL
         self.driver.maximize_window()
@@ -92,13 +95,12 @@ class Automation:
     def search(self, sn):
         try:
             wait = WebDriverWait(self.driver, self.config['page_load'])
-
             search = wait.until(EC.presence_of_element_located((By.ID, "barcodeTextField_searchCondition")))
             search.clear()
             search.send_keys(sn)
             search.send_keys(Keys.ENTER)
-            if self.__check_ticket_exists() is False:
-                raise ValueError("The ticket does not exist. Please search the Org SN# in the account Atravesar_01")
+            # if self.__check_ticket_exists() is False:
+            #     raise ValueError("The ticket does not exist. Please search the Org SN# in the account Atravesar_01")
             
         except Exception as search_error:
             #self.progress_queue.put(("log", f"Search error: {search_error}", "error"))
@@ -109,11 +111,15 @@ class Automation:
         with open(file_path, 'a', encoding='utf-8') as file:
             file.write(row)
     def close_ticket(self, batch_list):
+        if self.driver is None:
+            print("Driver not start")
+            return
         wait = WebDriverWait(self.driver, self.config['page_load'])
         # 1. Users using GUI to enter the scan org SN# and new SN# of the ticket.
         # 2. Users select the ticket type (e.g., Normal Pass, ECN Pass, Fail).
         # tuple list of (org_sn, new_sn, pass_type)
         for record in batch_list:
+            self.driver.refresh()
             org_sn, new_sn, pass_type = record
             try:
                 # Search by New SN# 
@@ -124,8 +130,14 @@ class Automation:
 
                 wait.until(EC.invisibility_of_element_located((By.CLASS_NAME, "MuiBackdrop-root-2")))
                 # If Org SN# and the input field of ORG SN# in UCS are not matched, it raises an error.
-                org_sn_in_page_element = wait.until(EC.presence_of_element_located((By.ID, "/repair/operation/rma/create/repair_common.static.general.repair_repair.static.repair.refurbish_orgSn")))
-                time.sleep(1)
+                iframes = self.driver.find_elements(By.TAG_NAME, "main")
+                if False:
+                    print(f"🧭 Found {len(iframes)} iframe(s), switching to the first one...")
+                    self.driver.switch_to.default_content()
+                time.sleep(2)
+                org_sn_in_page_element = wait.until(EC.presence_of_element_located((By.ID, "/repair/operation/rma/create/repair_common.static.general.repair_repair.static.repair.refurbish_orgSn")))        
+                #org_sn_in_page_element = wait.until(EC.presence_of_element_located((By.XPATH, "/html/body/div[2]/div[3]/main/div/div[2]/div[2]/div/div/div/div/div[1]/div/div[1]/div/div[2]/div/div/div/div[1]/div/div/div[1]/div/div/div/input")))
+                
                 org_sn_in_page = org_sn_in_page_element.get_attribute("value")
                 if org_sn_in_page.endswith(org_sn) is False:
                     print(f"Org SN# mismatch for ticket {new_sn}. Expected: {org_sn}, Found: {org_sn_in_page}")
@@ -173,15 +185,63 @@ class Automation:
                 print(f"Error processing ticket {new_sn}: {str(e)}")
                 traceback.print_exc()
 
-    def close_normal_ticket(self):
+    def __process_IRN(self):
         wait = WebDriverWait(self.driver, self.config['page_load'])
-        wait.until(EC.invisibility_of_element_located((By.CLASS_NAME, "MuiBackdrop-root-2")))
-        #RN not match: /repair/operation/rma/create/repair_common.static.general.repair_repair.static.repair.rn_repair.dic.rnMatch.match
         wait.until(EC.element_to_be_clickable((By.ID, "/repair/operation/rma/create/repair_common.static.general.repair_repair.static.repair.rn_repair.dic.rnMatch.notMatch"))).click()
-
-        #Save RN
+         #Save RN
         wait.until(EC.presence_of_element_located((By.ID, "/repair/operation/rma/create/repair_common.static.general.repair_repair.static.repair.rn_common.static.general.save"))).click()
-        time.sleep(self.config['request_delay'])  # Delay between requests
+
+    def __process_ECN(self):
+        try:
+            wait = WebDriverWait(self.driver, 10)
+        # Find the <tr> element
+            row_action = self.driver.find_element(By.XPATH, "/html/body/div[2]/div[3]/main/div/div[2]/div[2]/div/div/div/div/div[1]/div/div[3]/div/div[2]/div/div/div/div[1]/div/div/div/div[1]/div[2]/div/div/div/table/tbody/tr[1]")
+            # Get the text content of the element
+            if row_action is None:
+                return
+            row_text = row_action.text
+
+            # Check if "No Data" is present in the text
+            if "No Data" in row_text:
+                print("The <tr> element contains 'No Data'.")
+            else:
+                r = 0
+                while True:
+                    r += 1
+                    print(f"row: {r}")
+                    #row_action =     self.driver.find_element(By.XPATH, f"/html/body/div[2]/div[3]/main/div/div[2]/div[2]/div/div/div/div/div[1]/div/div[3]/div/div[2]/div/div/div/div[1]/div/div/div/div[1]/div[2]/div/div/div/table/tbody/tr[{r}]/td[7]/div/div/input")
+                    row_type = wait.until(EC.presence_of_element_located((By.XPATH, f"/html/body/div[2]/div[3]/main/div/div[2]/div[2]/div/div/div/div/div[1]/div/div[3]/div/div[2]/div/div/div/div[1]/div/div/div/div[1]/div[2]/div/div/div/table/tbody/tr[{r}]/td[2]")))
+                    
+                    print(row_type.text)
+                    if "Conditional" in row_type.text:
+                        print("Do not match condition")
+                        row_action = wait.until(EC.presence_of_element_located((By.XPATH, f"/html/body/div[2]/div[3]/main/div/div[2]/div[2]/div/div/div/div/div[1]/div/div[3]/div/div[2]/div/div/div/div[1]/div/div/div/div[1]/div[2]/div/div/div/table/tbody/tr[{r}]/td[7]/div/div/input")))
+                        if row_action is not None:
+                            break
+                        row_action.click()
+                        row_action.send_keys(Keys.DOWN)
+                        row_action.send_keys(Keys.DOWN)
+                        row_action.send_keys(Keys.ENTER)
+                    elif "Mandatory" in row_type.text:
+                        print("part shortage")
+                        row_action = wait.until(EC.presence_of_element_located((By.XPATH, f"/html/body/div[2]/div[3]/main/div/div[2]/div[2]/div/div/div/div/div[1]/div/div[3]/div/div[2]/div/div/div/div[1]/div/div/div/div[1]/div[2]/div/div/div/table/tbody/tr[{r}]/td[7]/div/div/input")))
+                        if row_action is not None:
+                            break
+                        row_action.click()
+                        row_action.send_keys(Keys.DOWN)
+                        row_action.send_keys(Keys.DOWN)
+                        row_action.send_keys(Keys.DOWN)
+                        row_action.send_keys(Keys.DOWN)
+                        row_action.send_keys(Keys.ENTER) #Part Shortage
+                    
+                    
+
+        except Exception as e:
+            print(f"An error occurred: {e}")
+            traceback.print_exc()
+    def __add_repair_detail(self, org_pn=""):
+        
+        wait = WebDriverWait(self.driver, self.config['page_load'])
         #Add Repair Detail: /repair/operation/rma/create/repair_common.static.general.repair_repair.static.repair.repairDetail_common.static.general.add
         wait.until(EC.presence_of_element_located((By.ID, "/repair/operation/rma/create/repair_common.static.general.repair_repair.static.repair.repairDetail_common.static.general.add"))).click()
 
@@ -200,30 +260,44 @@ class Automation:
         f2.send_keys(Keys.ENTER)
 
         f3 = wait.until(EC.presence_of_element_located((By.ID, "/repair/operation/rma/create/repair_Add Repair Detail_problemGroup")))
-        time.sleep(1)
+        time.sleep(0.5)
         f3.send_keys("[N0Z000] Test ok")
         f3.send_keys(Keys.DOWN)
         f3.send_keys(Keys.ENTER)
 
         f4 = wait.until(EC.presence_of_element_located((By.ID, "/repair/operation/rma/create/repair_Add Repair Detail_problemCode")))
-        time.sleep(1)
-        f4.send_keys("[N0Z000]Test ok")
+        time.sleep(0.5)
+        f4.send_keys("[N0Z000] Test ok")
         f4.send_keys(Keys.DOWN)
         f4.send_keys(Keys.ENTER)
 
         f5 = wait.until(EC.presence_of_element_located((By.ID, "/repair/operation/rma/create/repair_Add Repair Detail_actionGroup")))
+        time.sleep(0.5)
         f5.send_keys("[N] NTF")
         f5.send_keys(Keys.DOWN)
         f5.send_keys(Keys.ENTER)
 
         f6 = wait.until(EC.presence_of_element_located((By.ID, "/repair/operation/rma/create/repair_Add Repair Detail_actionCode")))
+        time.sleep(0.5)
         f6.send_keys("[N05] Cannot duplicate the symptom, and update Software/ Firmware/ BIOS") 
         f6.send_keys(Keys.DOWN)
         f6.send_keys(Keys.ENTER)
 
         #Save Repair Detail
-        save_detail = wait.until(EC.presence_of_element_located((By.ID, "/repair/operation/rma/create/repair_Add Repair Detail_common.static.general.submit")))
-        save_detail.click()
+        if False:
+            save_detail = wait.until(EC.presence_of_element_located((By.ID, "/repair/operation/rma/create/repair_Add Repair Detail_common.static.general.submit")))
+            save_detail.click()
+
+    def close_normal_ticket(self):
+        wait = WebDriverWait(self.driver, self.config['page_load'])
+        #self.__process_IRN()
+        time.sleep(self.config['request_delay'])
+        self.__process_ECN()
+        
+          # Delay between requests
+        time.sleep(self.config['request_delay'])
+        #self.__add_repair_detail()
+        
 
 
 
